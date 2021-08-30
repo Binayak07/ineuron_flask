6b923804881c2bc5aaf02756da1241ddd9811249
Binayak G Shankar <binayakgourishankar@gmail.com>
2021-08-30 19:02:13 +0530

    apis added

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, make_response
 import pymongo
 import sys
 import JSONEncoder
+import pandas as pd
+import json
 
 app = Flask(__name__)
 # mongoDB connection
@@ -37,5 +39,49 @@ def getAllDocuments():
     except Exception as e:
         return jsonify({"Msg":"Error"})
 
+@app.route('/mongo', methods=['DELETE'])
+def deleteDocument():
+    try:
+        id=request.json["id"]
+        condel = {"_id":id}
+        database["students"].delete_one(condel)
+        return jsonify({"Status":"Success","Msg":"Data deleted."})
+    except Exception as e:
+        return jsonify({"Msg":"Error"})
+
+@app.route('/mongo', methods=['PUT'])
+def updateDocument():
+    try:
+        id=request.json["id"]
+        newValue = request.json["nv"]
+        condel = {"_id":id}
+        database["students"].update_one(condel,newValue)
+        return jsonify({"Status":"Success","Msg":"Data updated."})
+    except Exception as e:
+        return jsonify({"Msg":"Error"})
+    
+@app.route('/mongo/bulk', methods=['POST'])
+def mongoBulkInsert():
+    try:
+        print(request.json['data'], file=sys.stderr)
+        # database["courses"].insert_many([])
+        return jsonify({"Status":"Success","Msg":"Data updated."})
+    except Exception as e:
+        return jsonify({"Msg":"Error"})
+    
+@app.route('/mongo/download', methods=['GET'])
+def downloadDocuments():
+    try:
+        allData = list(database["students"].find())
+        df=pd.DataFrame(allData)
+        f=json.dumps(allData)
+        resp = make_response(df.to_csv(JSONEncoder.JSONEncoder().encode(f)))
+        resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
+        resp.headers["Content-Type"] = "text/csv"
+        return resp
+    except Exception as e:
+        print(e,file=sys.stderr)
+        return jsonify({"Msg":"Error"})
+    
 if __name__ == '__main__':
     app.run()
